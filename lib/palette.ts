727248fa6e9abd8232330fc8d997f236117fe58b
Mahindra Chintala <127439352+ARGOD2213@users.noreply.github.com>
2026-09-24 03:36:47 +0530

    Add lib/palette.ts

---
+++ lib/palette.ts
@@ -0,0 +1,10 @@
+export const C = {
+  mint: '#BFE6D2',
+  chai: '#3B2114',
+  saucer: '#FFF9F0',
+  pink: '#F08AA5',
+  red: '#D3352B',
+  mintDeep: '#7FC4A6',
+  chaiLight: '#6B4028',
+  biscuit: '#D8B48A',
+} as const
